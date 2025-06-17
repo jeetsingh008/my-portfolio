b094528d6119dee23a22b2e76a9571683b4c9a38
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-06-17 14:08:48 +0530

    Added the redux setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import Navbar from "../../components/Navbar";
+import { store } from "../../redux/store/store";
+import { Provider } from "react-redux";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen bg-gray-dark font-poppins">
-      <Navbar />
-    </div>
+    <Provider store={store}>
+      <div className="w-full min-h-screen bg-gray-dark font-poppins">
+        <Navbar />
+      </div>
+    </Provider>
   );
 }
