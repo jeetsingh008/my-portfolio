f18924010a76eb1626b52730b138ff1bf7932d4e
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-07-06 19:27:31 +0530

    Added typewrite animation effect on CTA section text

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,14 +1,18 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import Typewriter from "typewriter-effect";
 import heroImage from "../../public/hero-banner.png";
 import { SlSocialFacebook } from "react-icons/sl";
 import { RiTwitterXFill } from "react-icons/ri";
 import { CiInstagram } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
+import { useAppSelector } from "../../redux/hooks";
 
 const HeroSection = () => {
+  const themeState = useAppSelector((state) => state.darkTheme.mode);
   return (
-    <div className="relative flex flex-col xl:flex-row justify-evenly items-center w-full">
+    <div className="relative flex flex-col md:flex-row justify-evenly items-center w-full">
       <div className="absolute top-1/2 -translate-y-1/2 left-0 flex flex-col items-center gap-4">
         <a
           href="#"
@@ -49,24 +53,40 @@ const HeroSection = () => {
         <Image
           alt="Hero Image"
           src={heroImage}
-          height={700}
-          width={700}
+          height={600}
+          width={600}
           priority
           style={{ objectFit: "cover" }}
         />
       </div>
       {/* Call to action part */}
-      <div className="max-xl:absolute max-xl:top-4/5 max-xl:-translate-y-4/5 flex justify-center items-center">
+      <div className="max-md:absolute max-md:top-4/5 max-md:-translate-y-4/5 flex justify-center items-center">
         <div className="flex-col items-center gap-3">
-          <p className="text-center text-4xl sm:text-6xl md:text-7xl xl:text-7xl font-semibold tracking-wide">
-            From <br /> <span>Concept</span>
+          <p
+            className={`text-center ${
+              themeState === "light" ? "text-white md:text-black" : ""
+            } text-4xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold tracking-wide`}
+          >
+            I{" "}
+            <span className="inline-block text-orange">
+              <Typewriter
+                options={{
+                  strings: ["Design", "Code", "Develop", "Create"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>{" "}
             <br />
-            to <br /> <span>Creation</span>
-            <span className="text-orange animate-pulse font-orbitron">.</span>
+            <h2 className="text-nowrap">
+              <span className="text-orange">You</span> grow
+              <span className="font-orbitron">.</span>
+            </h2>
           </p>
+
           <div className="flex justify-center items-center mt-6">
             <button className="border border-orange rounded-sm text-xl lg:text-2xl xl:text-3xl px-3 py-2 hover:bg-orange hover:text-gray-dark transition-colors duration-300 ease-in-out">
-              Contact Us
+              Contact
             </button>
           </div>
         </div>
